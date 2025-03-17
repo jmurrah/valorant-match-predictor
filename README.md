ad45ce4d6b531148c75c17c5b8b87eb84a3214f2
jmurrah <jacobhmurrah@gmail.com>
2025-03-17 13:38:58 -0500

    it HAS BEGUN

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # valorant-match-predictor
 I like valorant!
+
+## Creating Virtual Environment
+```
+python3 -m venv venv
+source venv/bin/activate
+```
+
+## Downloading Dependencies
+```
+pip3 install -r requirements.txt
+```
+
+## Updating Dependencies
+```
+pip3 freeze > requirements.txt
+```
+
+## Formatting
+```
+black .
+```
+
+## Deleting Virtual Environment
+```
+deactivate
+rm -rf venv
+```
+
+## Running Neural Network
+```
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,24 @@
+import torch
+from neural_network import NeuralNetwork
+
+
+def main():
+    print("Hello world!")
+    device = (
+        torch.accelerator.current_accelerator().type
+        if torch.accelerator.is_available()
+        else "cpu"
+    )
+    print(f"Using {device} device")
+    network = NeuralNetwork()
+    model = nn.to(device)
+
+    X = torch.rand(1, 28, 28, device=device)
+    logits = model(X)
+    # pred_probab = nn.Softmax(dim=1)(logits)
+    # y_pred = pred_probab.argmax(1)
+    # print(f"Predicted class: {y_pred}")
+
+
+if __name__ == "__main__":
+    main()
